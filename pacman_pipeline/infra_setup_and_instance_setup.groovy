d3e7b9f82a9ca9184a766f307c5b73298882a67b
ggg <ggg>
2023-12-01 13:07:17 -0500

    jhg

--- pacman_pipeline/infra_setup_and_instance_setup.groovy
+++ pacman_pipeline/infra_setup_and_instance_setup.groovy
@@ -71,22 +71,21 @@ pipeline {
             }
         }
     
-        stage('Run containers Pacman and Mongo DB') {
-            steps {
-                withCredentials([sshUserPrivateKey(credentialsId: 'TestInstance2Last', keyFileVariable: 'SSH_KEY')]) {
-                    sh '''
-                    cd /var/lib/jenkins/workspace/pacman_pipe/pacman_pipeline/ansible
-                    ansible all -i instance_ip.txt -m shell -a "docker-compose -f /home/ubuntu/docker-compose-deployment-pacman-and-mongo.yaml up -d" -u ubuntu --private-key=$SSH_KEY -e 'ansible_ssh_common_args="-o StrictHostKeyChecking=no"'
-                    ''' 
-
-                }
-    
-            }
-        }
-
     }
+    
 }
+        
+
+    
 
 
 
 
+
+ //stage('Run containers Pacman and Mongo DB') {
+            // steps {
+             //   withCredentials([sshUserPrivateKey(credentialsId: 'TestInstance2Last', keyFileVariable: 'SSH_KEY')]) {
+                //    sh '''
+                  //  cd /var/lib/jenkins/workspace/pacman_pipe/pacman_pipeline/ansible
+             //ansible all -i instance_ip.txt -m shell -a "docker-compose -f /home/ubuntu/docker-compose-deployment-pacman-and-mongo.yaml up -d" -u ubuntu --private-key=$SSH_KEY -e 'ansible_ssh_common_args="-o StrictHostKeyChecking=no"'
+                    //''' 
